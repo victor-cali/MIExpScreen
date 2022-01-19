c572a556d559d6973819fb9fee415152f3395e74
V. A. Canales L <victorcanlima@gmail.com>
2022-01-18 23:10:51 -0600

    Started application

---
+++ main.py
@@ -0,0 +1,10 @@
+import kivy
+from kivy.app import App
+
+class MIExpScreenApp(App):
+    
+    def build(self):
+        return super().build()
+
+if __name__ == '__main__':
+    MIExpScreenApp().run()
