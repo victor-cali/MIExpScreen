82a0de0e7a9de7470ab1db9423917355f089aa71
V. A. Canales L <victorcanlima@gmail.com>
2022-01-20 20:43:52 -0600

    Added LSL Markers Outlet

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from kivy.core.audio import SoundLoader
 from kivy.clock import Clock
 import numpy as np
+from pylsl import StreamInfo, StreamOutlet
+
+info = StreamInfo('MIExpScreenStim', 'Markers', 1, 0, 'int32', 'MIExpScreenApp')
+outlet = StreamOutlet(info)
 
 class SettingsWindow(Screen):
     def __init__(self, **kw):
         super(SettingsWindow, self).__init__(**kw)    
 
 class ExperimentWindow(Screen):
+
+    sound = SoundLoader.load('beep.wav')
     rng = np.random.default_rng()
+
     def __init__(self, settings, **kw):
         super(ExperimentWindow, self).__init__(**kw)
         self.classes_paths = settings['classes']
@@ -21,19 +29,26 @@ class ExperimentWindow(Screen):
         self.trials_num = settings['trials_num']
 
     def on_enter(self):
-        Clock.schedule_once(self.next_state, 10)
+        outlet.push_sample([0])
         classes = self.classes_paths.keys()
-        trials = self.rng.permutation([c for _ in range(self.trials_num) for c in classes])
+        trials = self.rng.permutation([c for _ in range(self.trials_num) for c in classes if c != 0])
         breaks = self.rng.integers(self.low_lim, self.top_lim, self.trials_num*len(classes), endpoint = True)
         self.trials = list(trials)
         self.breaks = list(breaks/10)
+        Clock.schedule_once(self.play_sound, 9)
+        Clock.schedule_once(self.next_state, 10)
+        print(self.trials)
         return super().on_enter()
+
+    def play_sound(self, dt):
+        self.sound.play()
     
     def next_state(self,dt):
         try:
             img_num = self.trials.pop()
             screen_img_src = self.classes_paths[img_num]
-            self.ids.screen_img.source = screen_img_src 
+            self.ids.screen_img.source = screen_img_src
+            outlet.push_sample([img_num])
             Clock.schedule_once(self.set_break_time, self.mi_len)
         except:
             pass
@@ -41,8 +56,10 @@ class ExperimentWindow(Screen):
     def set_break_time(self,dt):
         try:
             screen_img_src = self.classes_paths[0]
+            outlet.push_sample([0])
             self.ids.screen_img.source = screen_img_src
             break_time = self.breaks.pop() + self.mi_len
+            Clock.schedule_once(self.play_sound, break_time - 1)
             Clock.schedule_once(self.next_state, break_time)
         except:
             pass
